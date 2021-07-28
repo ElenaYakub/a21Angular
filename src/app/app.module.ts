4d5cf993e653f8b704407fcd822baeb3dab1aa7e
ElenaYakub <lightcharcoal7@gmail.com>
2021-07-28 21:41:12 +0300

    A_1 IUserModel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-//import { UserComponent } from './user/user.component';
 import { UsersComponent } from './users/users.component';
 import { UserComponent } from './user/user.component';
-import { ModelsComponent } from './models/models.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    //UserComponent,
     UsersComponent,
-    UserComponent,
-    ModelsComponent
+    UserComponent
   ],
   imports: [
     BrowserModule
