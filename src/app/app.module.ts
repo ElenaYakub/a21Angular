9bdef322dbef0ea2040607089fd7316fb050ff89
ElenaYakub <lightcharcoal7@gmail.com>
2021-07-28 16:56:40 +0300

    A_1 property

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+//import { UserComponent } from './user/user.component';
+import { UsersComponent } from './users/users.component';
+import { UserComponent } from './user/user.component';
+import { ModelsComponent } from './models/models.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    //UserComponent,
+    UsersComponent,
+    UserComponent,
+    ModelsComponent
   ],
   imports: [
     BrowserModule
