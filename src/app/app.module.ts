7743af2edfe13de4dc80d8abf5dbc8f059e1709a
ElenaYakub <lightcharcoal7@gmail.com>
2021-07-30 22:03:53 +0300

    ang_2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import {RouterModule} from "@angular/router";
+import { UsersComponent } from './users/users.component';
+import { PostsComponent } from './posts/posts.component';
+import {HttpClient, HttpClientModule} from "@angular/common/http";
 
-
-
+let routes = [
+  {path:'users', component: UsersComponent},
+  {path: 'posts', component: PostsComponent},
+];
 
 @NgModule({
   declarations: [
     AppComponent,
+    UsersComponent,
+    PostsComponent,
   ],
   imports: [
     BrowserModule,
-  ]
-  ,
+    RouterModule.forRoot([
+      {path:'users',component: UsersComponent},
+      {path:'posts',component: PostsComponent},
+    ]),
+    HttpClientModule
+  ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+}
